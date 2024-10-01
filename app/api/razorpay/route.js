ccb9b04000d6cbb224af6200e61025bc49c60b37
rxshabN <nagwanirishab@gmail.com>
2024-10-01 23:33:56 +0530

    fix: same as above

--- app/api/razorpay/route.js
+++ app/api/razorpay/route.js
@@ -3,62 +3,43 @@ import { validatePaymentVerification } from "razorpay/dist/utils/razorpay-utils"
 import Payment from "@/models/Payment";
 import connectDb from "@/db/connectDb";
 import User from "@/models/User";
+import PaymentSuccess from "@/app/paymentsuccess/page";
 
 export const POST = async (req) => {
   await connectDb();
+  let body = await req.formData();
+  body = Object.fromEntries(body);
 
-  try {
-    const body = await req.formData();
-    const bodyObject = Object.fromEntries(body);
+  let p = await Payment.findOne({ oid: body.razorpay_order_id });
 
-    const payment = await Payment.findOne({
-      oid: bodyObject.razorpay_order_id,
-    });
+  if (!p) {
+    return NextResponse.json({ success: false, message: "Order ID not found" });
+  }
 
-    if (!payment) {
-      return NextResponse.json({
-        success: false,
-        message: "Order ID not found",
-      });
-    }
+  let user = await User.findOne({ username: p.to_user });
 
-    const user = await User.findOne({ username: payment.to_user });
+  let isValid = validatePaymentVerification(
+    {
+      order_id: body.razorpay_order_id,
+      payment_id: body.razorpay_payment_id,
+    },
+    body.razorpay_signature,
+    process.env.KEY_SECRET
+  );
 
-    const isValid = validatePaymentVerification(
-      {
-        order_id: bodyObject.razorpay_order_id,
-        payment_id: bodyObject.razorpay_payment_id,
-      },
-      bodyObject.razorpay_signature,
-      process.env.KEY_SECRET
+  if (isValid) {
+    await Payment.findOneAndUpdate(
+      { oid: body.razorpay_order_id },
+      { done: true },
+      { new: true }
     );
-
-    if (isValid) {
-      await Payment.findOneAndUpdate(
-        { oid: bodyObject.razorpay_order_id },
-        { done: true },
-        { new: true }
-      );
-
-      // Ensure the URL is valid and accessible from the client (security best practice)
-      const redirectUrl = new URL(
-        "/paymentsuccess",
-        process.env.NEXT_PUBLIC_URL
-      );
-
-      return NextResponse.redirect(redirectUrl.toString());
-    } else {
-      return NextResponse.json({
-        success: false,
-        message: "Payment verification failed",
-      });
-    }
-  } catch (error) {
-    console.error("Error verifying payment:", error);
-    // Handle errors appropriately (e.g., log, return error response)
+    return NextResponse.redirect(
+      `${process.env.NEXT_PUBLIC_URL}/paymentsuccess`
+    );
+  } else {
     return NextResponse.json({
       success: false,
-      message: "Internal server error",
+      message: "Payment verification failed",
     });
   }
 };
