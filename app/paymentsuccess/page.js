6055e6c3fb62286e167afc75a0ee62cdf11cc5c9
rxshabN <nagwanirishab@gmail.com>
2024-10-02 22:05:09 +0530

    fix: redirect straight to login page rather than homepage

--- app/paymentsuccess/page.js
+++ app/paymentsuccess/page.js
@@ -1,95 +1,60 @@
 "use client";
-import { React, useEffect, useState } from "react";
+import { React, useEffect } from "react";
 import Link from "next/link";
 import { useSession } from "next-auth/react";
 import Loading from "../loading/page";
 import { useRouter } from "next/navigation";
-import { Flip, ToastContainer, toast } from "react-toastify";
-import "react-toastify/dist/ReactToastify.css";
 
 const PaymentSuccess = () => {
   const { data: session, status } = useSession();
   const router = useRouter();
-  const [showToast, setShowToast] = useState(false);
 
   useEffect(() => {
     if (status === "unauthenticated") {
-      router.push("/");
-      setShowToast(true);
+      router.push("/login"); // Redirect to the homepage
     }
   }, [status, router]);
-
-  useEffect(() => {
-    if (showToast) {
-      toast.info("Please signup/login first", {
-        position: "top-center",
-        autoClose: 1800,
-        hideProgressBar: false,
-        closeOnClick: true,
-        pauseOnHover: false,
-        draggable: false,
-        progress: undefined,
-        theme: "colored",
-        transition: Flip,
-      });
-    }
-  }, [showToast]);
 
   if (status === "loading") {
     return <Loading />;
   } else {
     return (
-      <>
-        <ToastContainer
-          position="top-center"
-          autoClose={1800}
-          hideProgressBar={false}
-          newestOnTop={false}
-          closeOnClick
-          rtl={false}
-          pauseOnFocusLoss={false}
-          draggable={false}
-          pauseOnHover={false}
-          theme="colored"
-          transition={Flip}
-        />
-        <div className="min-w-screen h-fit text-white text-4xl flex flex-col items-center relative top-52">
-          <div className="flex flex-col items-center gap-5 mb-5">
-            <div className="flex flex-row gap-5 items-center">
-              <svg
-                xmlns="http://www.w3.org/2000/svg"
-                viewBox="0 0 24 24"
-                width="140"
-                height="140"
-                color="#7ed321"
-                fill="none"
-              >
-                <path
-                  d="M22 12C22 6.47715 17.5228 2 12 2C6.47715 2 2 6.47715 2 12C2 17.5228 6.47715 22 12 22C17.5228 22 22 17.5228 22 12Z"
-                  stroke="currentColor"
-                  strokeWidth="1.5"
-                />
-                <path
-                  d="M8 12.5L10.5 15L16 9"
-                  stroke="currentColor"
-                  strokeWidth="1.5"
-                  strokeLinecap="round"
-                  strokeLinejoin="round"
-                />
-              </svg>
-            </div>
-            <div className="text-center">Payment Successful!</div>
-          </div>
-          <Link href="/">
-            <button
-              type="button"
-              className="text-white bg-gradient-to-br from-purple-600 to-blue-500 hover:bg-gradient-to-bl focus:ring-4 focus:outline-none focus:ring-blue-300 dark:focus:ring-blue-800 font-medium rounded-lg text-sm px-5 py-2.5 text-center me-2 mb-2"
+      <div className="min-w-screen h-fit text-white text-4xl flex flex-col items-center relative top-52">
+        <div className="flex flex-col items-center gap-5 mb-5">
+          <div className="flex flex-row gap-5 items-center">
+            <svg
+              xmlns="http://www.w3.org/2000/svg"
+              viewBox="0 0 24 24"
+              width="140"
+              height="140"
+              color="#7ed321"
+              fill="none"
             >
-              Go back to main page
-            </button>
-          </Link>
+              <path
+                d="M22 12C22 6.47715 17.5228 2 12 2C6.47715 2 2 6.47715 2 12C2 17.5228 6.47715 22 12 22C17.5228 22 22 17.5228 22 12Z"
+                stroke="currentColor"
+                strokeWidth="1.5"
+              />
+              <path
+                d="M8 12.5L10.5 15L16 9"
+                stroke="currentColor"
+                strokeWidth="1.5"
+                strokeLinecap="round"
+                strokeLinejoin="round"
+              />
+            </svg>
+          </div>
+          <div className="text-center">Payment Successful!</div>
         </div>
-      </>
+        <Link href="/">
+          <button
+            type="button"
+            className="text-white bg-gradient-to-br from-purple-600 to-blue-500 hover:bg-gradient-to-bl focus:ring-4 focus:outline-none focus:ring-blue-300 dark:focus:ring-blue-800 font-medium rounded-lg text-sm px-5 py-2.5 text-center me-2 mb-2"
+          >
+            Go back to main page
+          </button>
+        </Link>
+      </div>
     );
   }
 };
