b736ca25ca752806831d1e64df20a1589989c60c
rxshabN <nagwanirishab@gmail.com>
2024-10-02 21:57:03 +0530

    feat: added toast after redirect to homepage

--- app/paymentsuccess/page.js
+++ app/paymentsuccess/page.js
@@ -4,6 +4,8 @@ import Link from "next/link";
 import { useSession } from "next-auth/react";
 import Loading from "../loading/page";
 import { useRouter } from "next/navigation";
+import { Flip, ToastContainer, toast } from "react-toastify";
+import "react-toastify/dist/ReactToastify.css";
 
 const PaymentSuccess = () => {
   const { data: session, status } = useSession();
@@ -11,7 +13,18 @@ const PaymentSuccess = () => {
 
   useEffect(() => {
     if (status === "unauthenticated") {
-      router.push("/"); // Redirect to the homepage
+      toast.info("Please signup/login first", {
+        position: "top-center",
+        autoClose: 1800,
+        hideProgressBar: false,
+        closeOnClick: true,
+        pauseOnHover: false,
+        draggable: false,
+        progress: undefined,
+        theme: "colored",
+        transition: Flip,
+      });
+      router.push("/");
     }
   }, [status, router]);
 
@@ -19,42 +32,57 @@ const PaymentSuccess = () => {
     return <Loading />;
   } else {
     return (
-      <div className="min-w-screen h-fit text-white text-4xl flex flex-col items-center relative top-52">
-        <div className="flex flex-col items-center gap-5 mb-5">
-          <div className="flex flex-row gap-5 items-center">
-            <svg
-              xmlns="http://www.w3.org/2000/svg"
-              viewBox="0 0 24 24"
-              width="140"
-              height="140"
-              color="#7ed321"
-              fill="none"
-            >
-              <path
-                d="M22 12C22 6.47715 17.5228 2 12 2C6.47715 2 2 6.47715 2 12C2 17.5228 6.47715 22 12 22C17.5228 22 22 17.5228 22 12Z"
-                stroke="currentColor"
-                strokeWidth="1.5"
-              />
-              <path
-                d="M8 12.5L10.5 15L16 9"
-                stroke="currentColor"
-                strokeWidth="1.5"
-                strokeLinecap="round"
-                strokeLinejoin="round"
-              />
-            </svg>
+      <>
+        <ToastContainer
+          position="top-center"
+          autoClose={1800}
+          hideProgressBar={false}
+          newestOnTop={false}
+          closeOnClick
+          rtl={false}
+          pauseOnFocusLoss={false}
+          draggable={false}
+          pauseOnHover={false}
+          theme="colored"
+          transition={Flip}
+        />
+        <div className="min-w-screen h-fit text-white text-4xl flex flex-col items-center relative top-52">
+          <div className="flex flex-col items-center gap-5 mb-5">
+            <div className="flex flex-row gap-5 items-center">
+              <svg
+                xmlns="http://www.w3.org/2000/svg"
+                viewBox="0 0 24 24"
+                width="140"
+                height="140"
+                color="#7ed321"
+                fill="none"
+              >
+                <path
+                  d="M22 12C22 6.47715 17.5228 2 12 2C6.47715 2 2 6.47715 2 12C2 17.5228 6.47715 22 12 22C17.5228 22 22 17.5228 22 12Z"
+                  stroke="currentColor"
+                  strokeWidth="1.5"
+                />
+                <path
+                  d="M8 12.5L10.5 15L16 9"
+                  stroke="currentColor"
+                  strokeWidth="1.5"
+                  strokeLinecap="round"
+                  strokeLinejoin="round"
+                />
+              </svg>
+            </div>
+            <div className="text-center">Payment Successful!</div>
           </div>
-          <div className="text-center">Payment Successful!</div>
+          <Link href="/">
+            <button
+              type="button"
+              className="text-white bg-gradient-to-br from-purple-600 to-blue-500 hover:bg-gradient-to-bl focus:ring-4 focus:outline-none focus:ring-blue-300 dark:focus:ring-blue-800 font-medium rounded-lg text-sm px-5 py-2.5 text-center me-2 mb-2"
+            >
+              Go back to main page
+            </button>
+          </Link>
         </div>
-        <Link href="/">
-          <button
-            type="button"
-            className="text-white bg-gradient-to-br from-purple-600 to-blue-500 hover:bg-gradient-to-bl focus:ring-4 focus:outline-none focus:ring-blue-300 dark:focus:ring-blue-800 font-medium rounded-lg text-sm px-5 py-2.5 text-center me-2 mb-2"
-          >
-            Go back to main page
-          </button>
-        </Link>
-      </div>
+      </>
     );
   }
 };
