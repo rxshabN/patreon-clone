9a32a39b0ca4897f82e0c2e686dbfc3872a33082
rxshabN <nagwanirishab@gmail.com>
2024-10-02 16:04:24 +0530

    feat: added read more button on home page

--- app/page.js
+++ app/page.js
@@ -18,7 +18,7 @@ export default function Home() {
               type="button"
               className="mt-5 text-white bg-gradient-to-br from-purple-600 to-blue-500 hover:bg-gradient-to-bl focus:ring-4 focus:outline-none focus:ring-blue-300 dark:focus:ring-blue-800 font-medium rounded-lg text-lg px-5 py-2.5 text-center me-2 mb-2"
             >
-              About this project
+              Read more
             </button>
           </Link>
         </div>
