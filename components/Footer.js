8a316741f399494bc952f21630a0131957c298ea
rxshabN <nagwanirishab@gmail.com>
2024-11-20 19:30:59 +0530

    fixe build error

--- components/Footer.js
+++ components/Footer.js
@@ -1,11 +1,12 @@
 import React from "react";
+import Link from "next/link";
 
 const Footer = () => {
   return (
     <footer className="bg-black text-white px-4 flex flex-row items-center justify-center h-16 gap-12">
       <p className="text-center text-sm md:text-base">Made by Rishab Nagwani</p>
       <p className="text-center text-sm md:text-base">
-        <a href="/about">About this project</a>
+        <Link href="/about">About this project</Link>
       </p>
     </footer>
   );
